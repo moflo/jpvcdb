9eabee0fe0720d8afcc452183d8482d8f847102e
mobileflowllc <fieldforceapp@gmail.com>
2018-12-12 20:51:00 -0800

    working map with mapbox-gl

--- src/app/landing/Page2.js
+++ src/app/landing/Page2.js
@@ -2,6 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Row, Col, Table, Tag } from 'antd';
 import styled from 'styled-components';
+// import dynamic from 'next/dynamic'
+// const Map = dynamic(() => import('../credentials/mapbox'), {
+//   ssr: false
+// });
+
 
 const Page2Container = styled.div`
   margin-top: 30px;
@@ -12,8 +17,48 @@ const PageHeader = styled.h1`
     text-align: center;
 `
 
+var mapboxgl = {}
+if (process.browser) {
+  mapboxgl = require('mapbox-gl')
+}
 
-export default function Page2({ isMobile }) {
+mapboxgl.accessToken = 'pk.eyJ1IjoibW9mbG8iLCJhIjoidHo2M0hBMCJ9.W9Azve5aQ9VUNfI1dHOmvg'
+
+export default class Page2 extends React.PureComponent {
+  state = {
+    isFirstScreen: true,
+    isMobile: this.props.isMobile || false,
+    lng: -71.020000,
+    lat: 42.362400,
+    zoom: 10.0
+  };
+
+  componentDidMount() {
+    const { lng, lat, zoom } = this.state
+
+    if (process.browser) {
+    
+      const map = new mapboxgl.Map({
+        container: this.mapContainer,
+        style: 'mapbox://styles/moflo/cjpm243u904ue2smu3hhen462',
+        center: [lng, lat],
+        zoom
+      });
+    
+      map.on('move', () => {
+        const { lng, lat } = map.getCenter();
+    
+        // this.setState({
+        //   lng: lng.toFixed(4),
+        //   lat: lat.toFixed(4),
+        //   zoom: map.getZoom().toFixed(2)
+        // })
+      })
+        
+    }
+  }
+
+  render() {
 
   const DemoBox = props => <Card title={`Title: ${props.title}`}>{props.children}</Card>;
 
@@ -76,6 +121,7 @@ export default function Page2({ isMobile }) {
     tags: ['#b8daff', 'grey'],
   }];
   
+
   return (
     <Page2Container>
       <PageHeader>Page Two</PageHeader> 
@@ -88,11 +134,12 @@ export default function Page2({ isMobile }) {
             <Table columns={columns} dataSource={data} showHeader={false} size="small" pagination={false} bordered={false}/>
         </Col>
         <Col span={5}><DemoBox title="Testing2">
-        Chart One
+        <div ref={el => this.mapContainer = el} className="absolute top right left bottom" style={{ height: 300 }} />
         </DemoBox></Col>
       </Row>
     </Page2Container>
     );
+  }
 }
 Page2.propTypes = {
   isMobile: PropTypes.bool,
